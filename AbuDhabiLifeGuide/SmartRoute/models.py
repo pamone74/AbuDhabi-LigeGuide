58cdfa0f38bb47d2027bce25f7e23b0906758a67
Amone Patrick <140634743+pamone74@users.noreply.github.com>
2024-11-13 13:56:25 +0000

    Done

--- AbuDhabiLifeGuide/SmartRoute/models.py
+++ AbuDhabiLifeGuide/SmartRoute/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ParkingData(models.Model):
+    location = models.CharField(max_length=255)
+    available_spaces = models.IntegerField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class HealthCenter(models.Model):
+    name = models.CharField(max_length=255)
+    services = models.TextField()
+    rating = models.FloatField()
+    insurance_providers = models.TextField()
+    location = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+class EventData(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    category = models.CharField(max_length=255)
