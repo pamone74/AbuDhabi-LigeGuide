a1b021738bfbcc0c1f1114b14b2d7d0245bf7bcc
Amone Patrick <140634743+pamone74@users.noreply.github.com>
2024-11-19 03:13:18 +0000

    added some backend functionalities

--- AbuDhabiLifeGuide/AbuDhabiLifeGuide/urls.py
+++ AbuDhabiLifeGuide/AbuDhabiLifeGuide/urls.py
@@ -25,7 +25,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('map/', views.map_view, name='home'),
     path('', views.home, name='home'),
-     path('', views.map_view, name='home'),
-    path('SmartRoute/', include('SmartRoute.urls'))
+    path('', views.map_view, name='home'),
+    path('SmartRoute/', include('SmartRoute.urls')),
+    # path('recommend/<str:category>/', views.recommend_places, name="recommend_places"),
+    path("recommend-worship-church/", views.recommend_worship_church, name="recommend_worship_church"),
+    path("recommend-worship-mosque/", views.recommend_worship_mosque, name="recommend_worship_mosque"),
+    path("recommend-worship-shopping/", views.recommend_shopping_places, name="shopping"),
+    path("recommend-tourist-sites/", views.recommend_tourist_sites, name="recommend_tourist_sites"),
+    path("recommend-events/", views.recommend_events, name="event"),
+   path('book_parking_spot/<int:spot_id>/', views.book_parking_spot, name='book_parking_spot'),
+    path("booking/", views.parking_page, name="booking"),
 ]
+
 
--- AbuDhabiLifeGuide/SmartRoute/admin.py
+++ AbuDhabiLifeGuide/SmartRoute/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import ParkingSpot
+
+admin.site.register(ParkingSpot )
 
 # Register your models here.
